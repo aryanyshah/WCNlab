aa1778a2e292513db1b7ab7d7c3192db4b615ac5
shri3695 <shristishetty67@gmail.com>
2023-08-19 16:13:45 +0530

    js+photo

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ function validate(){
         phone=document.getElementById("phone").value;
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         var phoneno = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-        if( re.test(email) == false || email==""){
-            alert("Enter valid email");
-        }
-
-        else if (name1=="") {
+        if (name1=="") {
             alert( 'Name cannot be blank.');
         }
-
+        
         else if(phoneno.test(phone)==false || phone==""){
             alert("Enter valid Phone Number");
         }
+
+        else if( re.test(email) == false || email==""){
+            alert("Enter valid email");
+        }
+        
         else{
             alert("Your appointment is booked");
         }
